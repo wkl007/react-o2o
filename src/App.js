cc979cb4d51da46838d93628ed232c033e5564cd
wkl--007 <499657357@qq.com>
2018-05-16 15:53:26 +0800

    添加less支持，生产环境去除source-map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React
+            <span className="text">我是小黄片</span>
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
